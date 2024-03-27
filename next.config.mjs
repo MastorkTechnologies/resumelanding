2a86a818e6152aeed6148dc5cf8bb05ca3945349
Ashutosh Kewat <109691178+Ashu6200@users.noreply.github.com>
2024-03-27 18:14:05 +0530

    about had been added

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // domains: ["firebasestorage.googleapis.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-        pathname: "**",
-      },
-    ],
+    domains: ["firebasestorage.googleapis.com"],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "firebasestorage.googleapis.com",
+    //     pathname: "**",
+    //   },
+    // ],
   },
 };
 
